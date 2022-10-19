d6b33bc0d1e1bcaf058056e4c92f0f6079122fec
ameya-6964 <belvalkar7@gmail.com>
2022-10-19 10:02:43 +0530

    Completed Problem 2

---
+++ Array/problem2.js
@@ -0,0 +1,33 @@
+/*  QUESTION
+
+* Store a Set Of Words In An Array And Display The Content Both Forwards And Backwords  
+
+*/
+
+/* 
+* SOLUTION STARTS HERE 
+*/
+
+var Name = ["Ameya","Jayesh","Tushar","Suraj","Gaurav"];
+
+/* 
+ *  DISPLAYING FORWARD DIRECTION
+*/
+console.log("Name In Forward Direction : " + Name);
+
+/* 
+ *  DISPLAYING REVERSE DIRECTION
+*/
+console.log("Name In Reverse Direction : " + Name.reverse());
+
+
+/* 
+  * OUTPUT
+  * Name In Forward Direction : Ameya,Jayesh,Tushar,Suraj,Gaurav
+  * Name In Reverse Direction : Gaurav,Suraj,Tushar,Jayesh,Ameya
+  */
+
+
+
+
+
