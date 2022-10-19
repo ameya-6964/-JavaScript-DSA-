ad37d4a493772db87bddb43352f7c4a0b4dfe14b
ameya-6964 <belvalkar7@gmail.com>
2022-10-19 10:27:28 +0530

    Completed 3 Problem

---
+++ Array/problem3.js
@@ -0,0 +1,28 @@
+/*  QUESTION
+
+* Create an object that stores individual letters in an array and has a function for displaying the letters as a single word.
+
+*/
+
+/* 
+* SOLUTION STARTS HERE 
+*/
+
+class displayFullName {
+    constructor() {
+        this.Name1 = ['A', 'M', 'E', 'Y', 'A' ,'B','E','L','V','A','L','K','A','R'];
+        console.log(" Before Joining :- "+this.Name1);
+        this.makeFullName = function()
+        {
+             return this.Name1.join('');
+        }   
+    }
+}
+var Name = new displayFullName();
+console.log(" After Joining :- "+Name.makeFullName());
+
+/* 
+* OUTPUT
+* Before Joining :- A,M,E,Y,A,B,E,L,V,A,L,K,A,R
+* After Joining :- AMEYABELVALKAR
+*/
